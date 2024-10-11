6efb7f851eefe2a2a3e4b8b95d9f4ca2ba210c21
kawaiiow <poohsn1357@gmail.com>
2024-10-11 12:36:33 +0700

    speedrun

---
+++ index.js
@@ -0,0 +1,9 @@
+// const test = $("#test").cli
+
+// $("document").ready(function(){
+//     $("body").css("background-color", "red")
+// })
+
+// const user_input = () => {
+//     $(test).hide()
+// }
